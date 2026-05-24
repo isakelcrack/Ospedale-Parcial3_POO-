cbafb5584aace8b4ce2b54493a850ee2aa329b5a
isakelcrack <isaacu@uninorte.edu.co>
2026-05-24 13:43:07 -0500

    Revert "refactor: remove legacy packagee models"
    This reverts commit 12313cda2b5da66cc97d32f172cdb368384e35ca.

---
+++ src/packagee/HospitalizationStatus.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package packagee;
+
+/**
+ *
+ * @author edangulo
+ */
+public enum HospitalizationStatus {
+
+    REQUESTED,
+    ONGOING,
+    CANCELED
+
+}
